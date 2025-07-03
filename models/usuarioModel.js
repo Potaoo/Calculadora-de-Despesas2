1cd60e520fb1624f25a38995872153bfad595ce7
Caio Domingues <potaoo1030@gmail.com>
2025-07-03 19:15:14 -0300

    att

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,12 +1,12 @@
 const db = require('../db');
 
 async function criarUsuario(nome, email, senhaCriptografada) {
-  const [result] = await db.query('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senhaCriptografada]);
+  const [result] = await db.query('INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3)', [nome, email, senhaCriptografada]);
   return result.insertId;
 }
 
 async function buscarPorEmail(email) {
-  const [rows] = await db.query('SELECT * FROM usuarios WHERE email = ?', [email]);
+  const [rows] = await db.query('SELECT * FROM usuarios WHERE email = $1', [email]);
   return rows[0];
 }
 
